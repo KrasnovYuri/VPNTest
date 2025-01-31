3caf78df129dfe295f8adc092d5bf71a47e35ea3
KrasnovYuri <krasnov1yuri@gmail.com>
2025-01-31 16:01:19 +0300

    start

---
+++ VPNTest/VPNTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPNTestApp.swift
+//  VPNTest
+//
+//  Created by Юрий on 31.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct VPNTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
